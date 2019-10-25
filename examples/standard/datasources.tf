6740cb282ea26e409ae6c7be0700d163b7c56f64
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-10-25 12:51:11 -0400

    Fix: datasources and providers in example

--- examples/standard/datasources.tf
+++ examples/standard/datasources.tf
@@ -6,12 +6,4 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
-data "aws_eks_cluster" "this" {
-  name = module.standard.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.standard.cluster_name
-}
-
 data "aws_region" "current" {}
--- examples/standard/providers.tf
+++ examples/standard/providers.tf
@@ -5,18 +5,6 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
-provider "kubernetes" {
-  host                   = module.standard.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.standard.cluster_public_certificate)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
-  version                = "~> 1.9"
-}
-
 provider "random" {
   version = "~> 2.0"
-}
-
-provider "template" {
-  version = "~> 2.1.2"
 }
