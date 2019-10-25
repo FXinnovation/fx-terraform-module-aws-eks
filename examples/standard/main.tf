5af41b7e642ec8e592e3e3aef03e29c19931d768
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-10-25 12:39:26 -0400

    feature: Reflect module changes on example

--- examples/standard/main.tf
+++ examples/standard/main.tf
@@ -8,28 +8,8 @@ resource "random_string" "this" {
 module "standard" {
   source = "../../"
 
-  name                       = "terraform-jenkins-${random_string.this.result}"
-  master_iam_role_name       = "terraform-eks-cluster-${random_string.this.result}"
-  master_security_group_name = "aws-sg-eks-master-${random_string.this.result}"
-  master_security_group_tags = {
-    "Name" = "tooling-master-sg"
-  }
-  master_role_tags = {
-    "Name" = "tooling-master-role"
-  }
-  worker_iam_role_name          = "terraform-eks-nodes-${random_string.this.result}"
-  worker_instance_type          = "t2.small"
-  worker_name_prefix            = "terraform-eks-node-${random_string.this.result}-"
-  worker_autoscaling_group_name = "terraform-eks-asg-${random_string.this.result}"
-  worker_security_group_name    = "aws-sg-eks-nodes-${random_string.this.result}"
-  worker_iam_instance_profile   = "terraform-eks-node-${random_string.this.result}"
-  worker_use_max_pods           = false
-  worker_security_group_tags = {
-    "Name" = "tooling-worker-sg"
-  }
-  worker_role_tags = {
-    "Name" = "tooling-worker-role"
-  }
-  subnet_ids            = tolist(data.aws_subnet_ids.default.ids)
-  master_private_access = true
+  iam_role_name       = random_string.this.result
+  name                = random_string.this.result
+  security_group_name = random_string.this.result
+  subnet_ids          = tolist(data.aws_subnet_ids.default.ids)
 }
