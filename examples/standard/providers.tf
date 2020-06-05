c3a9f1fe73cff5a263748ac7749a095383f6701b
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2020-06-05 14:00:04 -0400

    maintenance: Fix deprecation warnings on interpolation

--- examples/standard/providers.tf
+++ examples/standard/providers.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version    = "~> 2.31.0"
   region     = "ca-central-1"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "kubernetes" {
